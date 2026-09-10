42b66ce76c3a8898742f1a81407ee5aecb02b24d
qq771319022-maker <qq771319022@gmail.com>
2026-09-10 12:12:45 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dc-eng-v163';
+const CACHE='dc-eng-v162';
 const CORE=['./','./index.html','./manifest.webmanifest','./vocab.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
@@ -7,10 +7,16 @@ self.addEventListener('fetch',e=>{
   const u=new URL(e.request.url);
   if(e.request.mode==='navigate'){
     e.respondWith(caches.match('./index.html').then(cached=>{
-      const update=fetch(e.request).then(r=>{
-        const cp=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',cp));return r;
-      }).catch(()=>cached);
-      return cached || update;
+      if(cached){
+        fetch(e.request).then(r=>{
+          const cp=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',cp));
+        }).catch(()=>{});
+        return cached;
+      }
+      return fetch(e.request).then(r=>{
+        const cp=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',cp));
+        return r;
+      });
     }));
     return;
   }
